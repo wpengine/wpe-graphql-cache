dc8f9be27acc336a83627035f9bb92cf5ea4c4a9
Mark Kelnar <749603+markkelnar@users.noreply.github.com>
2022-03-01 15:28:52 -0600

    Initial

---
+++ wpe-graphql.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Plugin Name:     WP Engine Graphql Add Ons
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     WP Engine add ons for wp-graphql in the WP Engine hosted WP environment
+ * Author:          Mark Kelnar
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wpe-graphql
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wpe_Graphql
+ */
+
+namespace WPEngine\Graphql;
+
+use WPGraphQL\Labs\Cache\Collection;
+
+/**
+ * For wpe, when our varnish cache function is invoked, add to paths being filtered.
+ * See the wpengine must-use plugin for the 'wpe_purge_varnish_cache_paths' filter.
+ * 
+ * @param array    $paths  Path, urls to pages cached in varnish to be purged.
+ * @param WP_Post  $post_id   Post object id.
+ */
+add_filter( 'wpe_purge_varnish_cache_paths', function ( $paths, $post_id ) {
+	
+	if ( ! is_array( $paths ) ) {
+		$paths = [];
+	}
+
+	// This purges all cached pages at graphql endpoint.
+	//$add_paths[] = Settings::graphql_endpoint();
+
+	// When any post changes, look up graphql paths previously queried containing post resources and purge those
+	$collection = new Collection();
+	$key   = $collection->nodes_key( 'post' );
+	$nodes = $collection->get( $key );
+
+	// Get the list of queries associated with this key
+	if ( is_array( $nodes ) ) {
+		foreach( $nodes as $request_key ) {
+			$urls_key = $collection->url_key( $request_key );
+			$urls = $collection->get( $urls_key );
+
+			if ( is_array( $urls ) ) {
+				// Add these specific paths to be purged
+				foreach ( $urls as $url ) {
+					// escape any regex characters
+					$paths[] = preg_quote( $url );
+				}
+			}
+		}
+	}
+
+	$paths[] = '/graphql\?queryId=bar10';
+	error_log( 'Graphql Purge Paths: ' . print_r($paths, 1) );
+	return array_unique( $paths );
+}, 10, 2 );
