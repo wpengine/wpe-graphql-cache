2e4c5fbd06e4aab82f1cf7b6b165ebf39b61d399
Mark Kelnar <749603+markkelnar@users.noreply.github.com>
2022-03-22 15:55:43 -0500

    Move graphql requests to helper class

--- tests/PostCacheCest.php
+++ tests/PostCacheCest.php
@@ -18,133 +18,22 @@
  */
 class PostCacheCest
 {
-	// The admin login token
-	public $auth_token;
-
 	// The post id created during tests, and needing cleanup
 	public $post_id;
-
-	// Clean up what we created at the end of all the tests in this suite
-	public $should_cleanup = true;
-
-	// Use this to delete the saved query
-	public $saved_query_id;
-
-	// Use this as the queryId
-	public $saved_query_alias;
 
 	// Use the following data on example post create/update
 	public $post_title = "Cache Test Runner";
 	public $post_content_0 = "initial content";
 	public $post_content_1 = "secondary data";
 
-	public function _login(AcceptanceTester $I)
-	{
-		// Login and save wordpress login token
-		if ( ! $this->auth_token ) {
-			$mutation = sprintf( "mutation LoginUser {
-				login( input: {
-					username: \"%s\",
-					password: \"%s\"
-				} ) {
-				authToken
-					user {
-						id
-						name
-					}
-				}
-			}
-			", $_ENV['WP_TEST_USERNAME'], $_ENV['WP_TEST_PASSWORD'] );
-
-			$I->sendPost('graphql', [ 'query' => $mutation ] );
-
-			// Save the token for other
-			$this->auth_token = $I->grabDataFromResponseByJsonPath('$..data.login.authToken')[0];
-			codecept_debug($this->auth_token);
-		}
-	}
-
-	/**
-	 * Create a saved query for use by this test runner
-	 */
-	public function SetUpTheSavedQuery( AcceptanceTester $I )
-	{
-		$this->_login( $I );
-
-		$I->haveHttpHeader( "Authorization", "Bearer " . $this->auth_token );
-
-		$mutation = 'mutation MyMutationInsert($input: CreateGraphqlDocumentInput!) {
-			createGraphqlDocument(input: $input) {
-			  graphqlDocument {
-				id
-			  }
-			}
-		  }
-		';
-		$this->saved_query_alias = uniqid( "test_runner_" );
-		$vars = [
-			"input" => [
-				"title" => "Saved query for test runner",
-				"content" => sprintf( 'query get_%s ($post: ID!) {
-					post(id: $post) {
-					  id
-					  title
-					  content
-					}
-				}', $this->saved_query_alias),
-				"alias" => [
-					$this->saved_query_alias
-				],
-				"status" => "PUBLISH",
-			]
-		];
-		
-		$I->sendPost('graphql', [ 'query' => $mutation, 'variables' => $vars ] );
-		$this->saved_query_id = $I->grabDataFromResponseByJsonPath('$..data.createGraphqlDocument.graphqlDocument.id')[0];
-	}
-
 	/**
 	 * While creating a post used for these other tests
 	*/
 	public function CreatePost( AcceptanceTester $I )
 	{
-		$this->_login( $I );
-
-		// Create a post
-		$I->haveHttpHeader( "Authorization", "Bearer " . $this->auth_token );
-		$mutation = 'mutation custom ($input: CreatePostInput!) {
-			createPost(input: $input) {
-			  post {
-				content
-				id
-				title
-			  }
-			}
-		  }
-		';
-		$category = "test-runner";
-		$vars = [
-			"input" => [
-				"title" => $this->post_title,
-				"content" => $this->post_content_0,
-				"status" => "PUBLISH",
-				"categories" => [
-					"nodes" => [
-						[
-							"name" => $category
-						]
-					],
-					"append" => false
-				]
-			]
-		];
-
-		$I->sendPost('graphql', [ 'query' => $mutation, 'variables' => $vars ] );
-		$response = $I->grabDataFromResponseByJsonPath('$..data.createPost.post')[0];
-		$this->post_id = $response['id'];
-
-		$I->assertEquals( $this->post_title, $response['title'] );
-		$I->assertEquals( "<p>{$this->post_content_0}</p>\n", $response['content'] );
+		$post['title'] = $this->post_title;
+		$post['content'] = $this->post_content_0;
+		$this->post_id = $I->havePost( $post );
 	}
 
 	/**
@@ -152,17 +41,15 @@ class PostCacheCest
 	 */
 	public function VerifySeeCacheHeaderTest( AcceptanceTester $I )
 	{
-		// Query the specific post with GET
-		$vars = [ "post" => $this->post_id ];
-		$I->sendGet('graphql', [ 'queryId' => $this->saved_query_alias, 'variables' => $vars ] );
-		$response = $I->grabDataFromResponseByJsonPath('$..data.post')[0];
+		$I->seePostById( $this->post_id );
+		$response = $I->grabDataByPost();
 		$I->assertEquals( $this->post_title, $response['title'] );
 		$I->assertEquals( "<p>{$this->post_content_0}</p>\n", $response['content'] );
 		$I->seeHttpHeader('X-Cache', 'MISS');
 
 		// Query again and see the cached version
-		$I->sendGet('graphql', [ 'queryId' => $this->saved_query_alias, 'variables' => $vars ] );
-		$response = $I->grabDataFromResponseByJsonPath('$..data.post')[0];
+		$I->seePostById( $this->post_id );
+		$response = $I->grabDataByPost();
 		$I->assertEquals( $this->post_title, $response['title'] );
 		$I->assertEquals( "<p>{$this->post_content_0}</p>\n", $response['content'] );
 		$I->seeHttpHeader('X-Cache', 'HIT: 1');
@@ -173,28 +60,8 @@ class PostCacheCest
 	 */
 	public function UpdateThePostTest( AcceptanceTester $I )
 	{
-		// Update the post
-		$I->haveHttpHeader( "Authorization", "Bearer " . $this->auth_token );
-		$mutation = 'mutation custom ($update: UpdatePostInput!) {
-			updatePost(input: $update) {
-			  post {
-				id
-				title
-				content  
-			  }
-			}
-		  }
-		';
-
-		$vars = [
-			"update" => [
-				"id" => $this->post_id,
-				"content" => $this->post_content_1,
-			]
-		];
-		$I->sendPost('graphql', [ 'query' => $mutation, 'variables' => $vars ] );
-		$response = $I->grabDataFromResponseByJsonPath('$..data.updatePost.post')[0];
-		$I->assertEquals( "<p>{$this->post_content_1}</p>\n", $response['content'] );
+		$params['content'] = $this->post_content_1;
+		$I->updatePost( $this->post_id, $params );
 	}
 
 	/**
@@ -202,55 +69,15 @@ class PostCacheCest
 	 */
 	public function CheckForCacheMissTest( AcceptanceTester $I )
 	{
-		// Query and check for cache miss
-		$vars = [ "post" => $this->post_id ];
-		$I->sendGet('graphql', [ 'queryId' => $this->saved_query_alias, 'variables' => $vars ] );
-		$response = $I->grabDataFromResponseByJsonPath('$..data.post')[0];
-
+		$I->seePostById( $this->post_id );
 		$I->seeHttpHeader('X-Cache', 'MISS');
+		$response = $I->grabDataByPost();
 		$I->assertEquals( $this->post_title, $response['title'] );
 		$I->assertEquals( "<p>{$this->post_content_1}</p>\n", $response['content'] );
 	}
 
-	/**
-	 * At the end of all tests, call this teardown
-	 */
-	public function tearDown(AcceptanceTester $I)
+	public function CleanUpAtTheEnd( AcceptanceTester $I )
 	{
-		codecept_debug("TEARDOWN");
-
-		if ( $this->should_cleanup ) {
-			codecept_debug( "Cleanup, after" );
-	
-			$this->_login( $I );
-			$I->haveHttpHeader( "Authorization", "Bearer " . $this->auth_token );
-
-			if ( $this->post_id ) {
-				codecept_debug( $this->post_id );
-				$mutation = 'mutation deletePost($var: DeletePostInput!) { deletePost (input: $var) { deletedId } }';
-				$vars = [
-					"var" => [
-						"id" => $this->post_id
-					]
-				];
-				$I->sendPost('graphql', [ 'query' => $mutation, 'variables' => $vars ] );
-				codecept_debug( $I->grabResponse() );
-				$this->post_id = null;
-			}
-
-			if ( $this->saved_query_id ) {
-				codecept_debug( $this->saved_query_id );
-				$mutation = 'mutation deleteSavedQuery($var: DeleteGraphqlDocumentInput!) { deleteGraphqlDocument (input: $var) graphqlDocument { id } } ';
-				$vars = [
-					"var" => [
-						"id" => $this->saved_query_id
-					]
-				];
-				$I->sendPost('graphql', [ 'query' => $mutation, 'variables' => $vars ] );
-				codecept_debug( $I->grabResponse() );
-				$this->saved_query_id = null;
-			}
-		}
+		$I->cleanUp();
 	}
-
 }
--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -1,10 +1,243 @@
 <?php
 namespace Helper;
 
-// here you can define custom actions
+/**
+ * Extend the acceptance tester to send Rest requests to the graphql endpoint.
+ * Handle login and save those auth creds as well as add to requests.
+ */
+
+ // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
 class Acceptance extends \Codeception\Module
 {
+	// The admin login token
+	public $auth_token;
 
+	// Clean up what we created at the end of all the tests in this suite
+	public $should_cleanup = true;
+
+	// Use this to delete the saved query
+	public $saved_query_id;
+
+	// Use this as the queryId
+	public $saved_query_alias;
+
+	// The post id created during tests, and needing cleanup
+	public $created_post_id;
+
+	public function _beforeSuite($settings = [])
+	{
+		$this->login();
+		$this->saved_query_alias = uniqid( "test_runner_" );
+		$this->haveQueryForSinglePost();
+
+	}
+
+	public function login()
+	{
+		// Login and save wordpress login token
+		if ( ! $this->auth_token ) {
+			$mutation = sprintf( "mutation LoginUser {
+				login( input: {
+					username: \"%s\",
+					password: \"%s\"
+				} ) {
+					authToken
+				}
+			}
+			", $_ENV['WP_TEST_USERNAME'], $_ENV['WP_TEST_PASSWORD'] );
+
+			$this->getModule('REST')->sendPost('graphql', [ 'query' => $mutation ] );
+			$this->getModule('REST')->seeResponseCodeIs(200);
+
+			// Save the token for other
+			$this->auth_token = $this->getModule('REST')->grabDataFromResponseByJsonPath('$..data.login.authToken')[0];
+		}
+	}
+
+	/**
+	 * Create a saved query for use by this test runner
+	 */
+	public function haveQueryForSinglePost()
+	{
+		if ( ! $this->saved_query_id ) {
+			$mutation = 'mutation MyMutationInsert($input: CreateGraphqlDocumentInput!) {
+				createGraphqlDocument(input: $input) {
+				graphqlDocument {
+					id
+				}
+				}
+			}
+			';
+			$vars = [
+				"input" => [
+					"title" => "Saved query for test runner",
+					"content" => sprintf( 'query get_%s ($post: ID!) {
+						post(id: $post) {
+						id
+						title
+						content
+						}
+					}', $this->saved_query_alias),
+					"alias" => [
+						$this->saved_query_alias
+					],
+					"status" => "PUBLISH",
+				]
+			];
+
+			$this->sendAuthenticatedPost('graphql', [ 'query' => $mutation, 'variables' => $vars ] );
+			$this->saved_query_id = $this->getModule('REST')->grabDataFromResponseByJsonPath('$..data.createGraphqlDocument.graphqlDocument.id')[0];
+		}
+	}
+
+	/**
+	 * Create a WP post used for these test scenarios. Will be cleaned up after the suite run completes.
+	 *
+	 * param $params
+	 *  'title' - post_title
+	 *  'content' - post_content
+	 *  '
+	 */
+	public function havePost( $params = [] )
+	{
+		$category = "test-runner";
+		$mutation = 'mutation custom ($vars: CreatePostInput!) {
+			createPost(input: $vars) {
+			  post {
+				id
+				title
+				content
+			  }
+			}
+		  }
+		';
+		$vars = [
+			"vars" => [
+				"title" => $params['title'],
+				"content" => $params['content'],
+				"status" => "PUBLISH",
+				"categories" => [
+					"nodes" => [
+						[
+							"name" => $category
+						]
+					],
+					"append" => false
+				]
+			]
+		];
+
+		$this->sendAuthenticatedPost('graphql', [ 'query' => $mutation, 'variables' => $vars ] );
+		$response = $this->getModule('REST')->grabDataFromResponseByJsonPath('$..data.createPost.post')[0];
+		$this->created_post_id = $response['id'];
+
+		$this->getModule('Asserts')->assertEquals( $params['title'], $response['title'] );
+		$this->getModule('Asserts')->assertEquals( "<p>{$params['content']}</p>\n", $response['content'] );
+
+		return $this->created_post_id;
+	}
+
+	public function updatePost( $post_id, $params )
+	{
+		$mutation = 'mutation custom ($vars: UpdatePostInput!) {
+			updatePost(input: $vars) {
+			  post {
+				id
+				title
+				content
+			  }
+			}
+		  }
+		';
+
+		$vars = [
+			"vars" => [
+				"id" => $post_id,
+				"content" => $params['content'],
+			]
+		];
+		$this->sendAuthenticatedPost('graphql', [ 'query' => $mutation, 'variables' => $vars ] );
+		$response = $this->getModule('REST')->grabDataFromResponseByJsonPath('$..data.updatePost.post')[0];
+		$this->getModule('Asserts')->assertEquals( "<p>{$params['content']}</p>\n", $response['content'] );
+
+	}
+
+	/**
+	 * Query graphql server and verify single post id exists.
+	 * Uses saved queryId mutation.
+	 *
+	 * param $url
+	 * param array $params
+	 */
+	public function seePostById( $post_id )
+	{
+		$vars = [ "post" => $post_id ];
+		$this->getModule('REST')->sendGet('graphql', [ 'queryId' => $this->saved_query_alias, 'variables' => $vars ] );
+	}
+
+	/**
+	 * Return the single post from the graphql query.
+	 *
+	 * return string
+	 */
+	public function grabDataByPost()
+	{
+		return $this->getModule('REST')->grabDataFromResponseByJsonPath('$..data.post')[0];
+	}
+
+	/**
+	 * Sends a POST request to given uri with authorization bearer token from login().
+	 * Assumes login() already invoked and successful.
+	 *
+	 * param $url
+	 * param array|string|\JsonSerializable $params
+	 */
+	public function sendAuthenticatedPost( $url, $params )
+	{
+		$this->getModule('REST')->haveHttpHeader( "Authorization", "Bearer " . $this->auth_token );
+		$this->getModule('REST')->sendPost( $url, $params );
+		$this->getModule('REST')->seeResponseCodeIs(200);
+	}
+
+	/**
+	 * Call at the end of the suite run
+	 */
+	public function cleanUp()
+	{
+		codecept_debug("TEARDOWN");
+
+		if ( $this->should_cleanup ) {
+			codecept_debug( "Cleanup, after" );
+
+			if ( $this->created_post_id ) {
+				$mutation = 'mutation deletePost($var: DeletePostInput!) { deletePost (input: $var) { deletedId } }';
+				$vars = [
+					"var" => [
+						"id" => $this->created_post_id
+					]
+				];
+				codecept_debug( "Cleanup, after" );
+				$this->sendAuthenticatedPost('graphql', [ 'query' => $mutation, 'variables' => $vars ] );
+				codecept_debug( $this->getModule('REST')->grabResponse() );
+				$this->created_post_id = null;
+			}
+
+			if ( $this->saved_query_id ) {
+				codecept_debug( $this->saved_query_id );
+				$mutation = 'mutation deleteSavedQuery($var: DeleteGraphqlDocumentInput!) { deleteGraphqlDocument (input: $var) { graphqlDocument { id } } }';
+				$vars = [
+					"var" => [
+						"id" => $this->saved_query_id
+					]
+				];
+				codecept_debug( "Cleanup, after" );
+				$this->sendAuthenticatedPost('graphql', [ 'query' => $mutation, 'variables' => $vars ] );
+				codecept_debug( $this->getModule('REST')->grabResponse() );
+				$this->saved_query_id = null;
+			}
+		}
+
+	}
 }
